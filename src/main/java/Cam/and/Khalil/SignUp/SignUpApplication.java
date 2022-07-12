ea1c5c22513a7359262df6f485ffa05e9d44f1b3
Cameron WIlson <cawil29@morgan.edu>
2022-07-12 16:05:44 -0400

    push

---
+++ src/main/java/Cam/and/Khalil/SignUp/SignUpApplication.java
@@ -0,0 +1,13 @@
+package Cam.and.Khalil.SignUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Cam/and/Khalil/SignUp/SignUpApplicationTests.java
@@ -0,0 +1,13 @@
+package Cam.and.Khalil.SignUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
